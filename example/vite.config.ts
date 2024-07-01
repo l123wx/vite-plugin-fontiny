0eddf27c7fbc01496509d73cfcf45613f301b1b1
liao文禧 <1219585136@qq.com>
2024-07-02 00:26:24 +0800

    ci: fixed example building error

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import Inspect from 'vite-plugin-inspect'
-// import Fontiny from '../src/index'
-import Fontiny from '../'
+import Fontiny from '../src/index'
+// import Fontiny from '../'
 
 export default defineConfig({
   base: './',
